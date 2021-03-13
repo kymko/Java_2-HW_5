265a98cb6fc36d3ad991ed2f2c12e9ff767fc540
kymko <Kymkobek@gmail.com>
2021-03-14 01:26:44 +0300

    исправил и сократил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,62 +10,45 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        enterNumber(arrayList1);
+
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        enterNumber(arrayList2);
+
+        ArrayList<String> arrayList3 = new ArrayList<>();
+        arrayList3.addAll(Collections.singleton(arrayList1.get(0) + " - " + arrayList2.get(4)));
+        arrayList3.addAll(Collections.singleton(arrayList1.get(1) + " - " + arrayList2.get(3)));
+        arrayList3.addAll(Collections.singleton(arrayList1.get(2) + " - " + arrayList2.get(2)));
+        arrayList3.addAll(Collections.singleton(arrayList1.get(3) + " - " + arrayList2.get(1)));
+        arrayList3.addAll(Collections.singleton(arrayList1.get(4) + " - " + arrayList2.get(0)));
+        System.out.println(arrayList3);
+
+    }
+
+    public static void enterNumber(ArrayList list) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 5 words");
         String str0 = scanner.nextLine();
-        ArrayList<String> list1 = new ArrayList<>(5);
-        list1.add(str0);
-        info(list1);
+        list.add(str0);
+        info(list);
         System.out.println("Enter 1 word");
         String str1 = scanner.nextLine();
-        list1.add(str1);
-        info(list1);
+        list.add(str1);
+        info(list);
         System.out.println("Enter 1 word");
         String str2 = scanner.nextLine();
-        list1.add(str2);
-        info(list1);
+        list.add(str2);
+        info(list);
         System.out.println("Enter 1 word");
         String str3 = scanner.nextLine();
-        list1.add(str3);
-        info(list1);
+        list.add(str3);
+        info(list);
         System.out.println("Enter 1 word");
         String str4 = scanner.nextLine();
-        list1.add(str4);
-        info(list1);
-
-
-        ArrayList<String> list2 = new ArrayList<>(5);
-        System.out.println("Enter 5 another words");
-        String str5 = scanner.nextLine();
-        list2.add(str5);
-        info(list2);
-        System.out.println("Enter 1 another word");
-        String str6 = scanner.nextLine();
-        list2.add(str6);
-        info(list2);
-        System.out.println("Enter 1 another word");
-        String str7 = scanner.nextLine();
-        list2.add(str7);
-        info(list2);
-        System.out.println("Enter 1 another word");
-        String str8 = scanner.nextLine();
-        list2.add(str8);
-        info(list2);
-        System.out.println("Enter 1 another word");
-        String str9 = scanner.nextLine();
-        list2.add(str9);
-        info(list2);
-
-
-        ArrayList<String> list3 = new ArrayList<>();
-        list3.addAll(Collections.singleton(str0 + " - " + str9));
-        list3.addAll(Collections.singleton(str1 + " - " + str8));
-        list3.addAll(Collections.singleton(str2 + " - " + str7));
-        list3.addAll(Collections.singleton(str3 + " - " + str6));
-        list3.addAll(Collections.singleton(str4 + " - " + str5));
-
-
-        info(list3);
-
+        list.add(str4);
+        info(list);
 
     }
 
