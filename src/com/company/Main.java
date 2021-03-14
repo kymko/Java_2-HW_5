7ca2dff760465e956c830dacd9ee78fcbbd87cd2
kymko <Kymkobek@gmail.com>
2021-03-14 03:19:30 +0300

    изменил метод и добавил сортировку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,10 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> arrayList1 = new ArrayList<>();
         enterNumber(arrayList1);
@@ -18,38 +13,32 @@ public class Main {
         enterNumber(arrayList2);
 
         ArrayList<String> arrayList3 = new ArrayList<>();
-        arrayList3.addAll(Collections.singleton(arrayList1.get(0) + " - " + arrayList2.get(4)));
-        arrayList3.addAll(Collections.singleton(arrayList1.get(1) + " - " + arrayList2.get(3)));
-        arrayList3.addAll(Collections.singleton(arrayList1.get(2) + " - " + arrayList2.get(2)));
-        arrayList3.addAll(Collections.singleton(arrayList1.get(3) + " - " + arrayList2.get(1)));
-        arrayList3.addAll(Collections.singleton(arrayList1.get(4) + " - " + arrayList2.get(0)));
+        Collections.reverse(arrayList2);
+        Iterator<String> iterator = arrayList2.iterator();
+        while (iterator.hasNext()) {
+            Iterator<String> iterator1 = arrayList1.iterator();
+            while (iterator.hasNext()) {
+                arrayList3.add(iterator1.next());
+                iterator1.remove();
+                break;
+            }
+            arrayList3.add(iterator.next());
+        }
         System.out.println(arrayList3);
-
+        System.out.println("list sorted!");
+        arrayList3.sort(Comparator.comparing((String::length)));
+        System.out.println(arrayList3);
     }
 
     public static void enterNumber(ArrayList list) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 5 words");
-        String str0 = scanner.nextLine();
-        list.add(str0);
+        while (list.size() < 5){
+            String str0 = scanner.nextLine();
+            list.add(str0);
+        }
         info(list);
-        System.out.println("Enter 1 word");
-        String str1 = scanner.nextLine();
-        list.add(str1);
-        info(list);
-        System.out.println("Enter 1 word");
-        String str2 = scanner.nextLine();
-        list.add(str2);
-        info(list);
-        System.out.println("Enter 1 word");
-        String str3 = scanner.nextLine();
-        list.add(str3);
-        info(list);
-        System.out.println("Enter 1 word");
-        String str4 = scanner.nextLine();
-        list.add(str4);
-        info(list);
-
+        System.out.println("----------------------------------");
     }
 
     public static void info(ArrayList<?> arrayList) {
